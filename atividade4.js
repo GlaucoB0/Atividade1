46b5a50f50b225ab9da94b8fd6698da5defe359c
Glauco2E <oniiichaan61@gmail.com>
2023-07-27 09:15:15 -0300

    atividade 4 padronizada

--- atividade4.js
+++ atividade4.js
@@ -1,5 +1,12 @@
-let unidade = prompt("Digite o valor do produto: ")
-parseFloat(unidade)
+//1º declarar as variaveis
+let valor, precoDesconto
 
-let valor = unidade*2
+//2º variaveis que irão receber valor
+valor = Number(prompt("Digite o valor do produto: "))
+
+//3º processar as variaveis
+precoDesconto = Math.floor(valor*2)
+
+//4º mostrar resultado
+document.write(`O valor final ${precoDesconto}`)
 
